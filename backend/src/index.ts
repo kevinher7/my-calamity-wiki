8365e5af4af30be324d4e298ae9d60781a0434dc
Kevin Hernandez <kevinhernem@gmail.com>
2024-11-22 17:42:54 +0900

    Added a get method to access the class setups database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
+import * as fs from "fs";
+
 import express, { Request, Response } from "express";
 import { scrapeWiki } from "./utils/scrapeWiki";
 
 const app = express();
 const PORT = 8050;
 
-app.patch("/api/update-wiki-data", (req: Request, res: Response) => {
+app.get("/api/get-class-setups", (req: Request, res: Response) => {
+	const classSetups = {
+		preHardmode: JSON.parse(
+			fs.readFileSync("./db/preHardmodeClassSetupsData.json", "utf-8")
+		),
+		hardmode: JSON.parse(
+			fs.readFileSync("./db/hardmodeClassSetupsData.json", "utf-8")
+		),
+		postMoonLord: JSON.parse(
+			fs.readFileSync("./db/postMoonLordClassSetupsData.json", "utf-8")
+		),
+	};
+
+	res.send(classSetups);
+});
+
+app.patch("/api/update-setups-data", (req: Request, res: Response) => {
 	scrapeWiki();
 	res.statusCode;
 });
