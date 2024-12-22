9613c09a2706e1d38c69c416237f19b8455f4843
Kevin Hernandez <kevinhernem@gmail.com>
2024-12-22 23:40:35 +0900

    regactor: add a removeUnderscores util to use when parsing some of the data

--- frontend/src/components/SetupComponent/index.tsx
+++ frontend/src/components/SetupComponent/index.tsx
@@ -4,6 +4,7 @@ import { createSetupTableRowData } from "../../utils/createSetupTable";
 import createAnchorElement from "../../utils/createAnchorElement";
 
 import "./styles.css";
+import { removeUnderscores } from "../../utils/strings";
 
 interface SetupsComponentProps {
 	gameState: string;
@@ -13,6 +14,7 @@ interface SetupsComponentProps {
 const SetupComponent = (props: SetupsComponentProps) => {
 	return (
 		<div className="setup-component">
+			<h1 className="setup-h1">{removeUnderscores(props.gameState)}</h1>
 			<h1 className="setup-h1">{props.gameState}</h1>
 			<table className="setup-table">
 				<thead>
--- frontend/src/utils/strings.ts
+++ frontend/src/utils/strings.ts
@@ -1,3 +1,7 @@
 export const capitalizeFirstLetter = (word: string) => {
 	return word.charAt(0).toUpperCase() + word.slice(1);
 };
+
+export const removeUnderscores = (word: string) => {
+	return word.replace("_", " ");
+};
